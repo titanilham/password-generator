4a012778fd29ba702caee8ef3a1c18f1ae5181ca
Ilham <86422270+titanilham@users.noreply.github.com>
2024-07-22 19:08:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,10 +29,6 @@ def send_msg(text):
     url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text
     results = requests.get(url_req)
     print(results.json())
-    
 
 
-send_msg(password_generator("Vk", 20))
-send_msg(password_generator("Telegram", 18))
-send_msg(password_generator("Instagram", 19))
 send_msg(password_generator("GitHub", 25))
