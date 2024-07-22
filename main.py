5d16c12c2ffd3987fc89d7578c042d038ee1d559
Ilham <86422270+titanilham@users.noreply.github.com>
2024-07-22 18:42:49 +0300

    Added password generation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
 
-def password_generator(name: str, length: int) -> str:
+def password_generator(name="Name", length="18") -> str:
+    
+    name = input("Name: ")
+    length = int(input("Length: "))
     
     x = ''
     
@@ -11,9 +14,11 @@ def password_generator(name: str, length: int) -> str:
             x = x + random.randint(0,9)
             
         else:
-            x = x + chr(random.randint(32, 126))
-        
-    return x 
+            x = x + chr(random.randint(33, 126))
+    
+    x = f"\033[1m{name}:\033[0m \x1B[3m{x}\x1B[0m"  
+    
+    return x
 
-print(password_generator("Vk", 15))
-            
+print(password_generator())
+
